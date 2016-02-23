695c7402b7cbf0da9a0f55fb58ee39b7822e5432
andreuxsx <jedimaul600@gmail.com>
2016-02-23 12:01:07 -0400

    Added files via upload

---
+++ ProgSuma.cpp
@@ -0,0 +1,19 @@
+// ProgSuma.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include "conio.h"
+using namespace std;
+
+int main()
+{   
+	int a,b,Suma;
+	cin >> a;
+	cin >> b;
+	Suma = a+b;
+	cout << Suma << endl;
+	getch();
+	return 0;
+}
+
