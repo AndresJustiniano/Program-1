fb1c175482a2cfcfc4aef45f9315f22225a58b04
Andres Justiniano <jedimaul600@gmail.com>
2016-04-12 11:32:11 -0400

    Added files via upload

---
+++ factorialvalidacion.cpp
@@ -0,0 +1,28 @@
+// factorialvalidacion.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include "conio.h"
+
+using namespace std;
+
+void main()
+{
+	int factorial,i,n;
+	// factorial es un acumulador de producto
+	// i es un contador
+    // n es el numero al que se necesita calcular el factorial
+	// validar la entrada
+	factorial=1; //iniciar el acumulador de producto
+	do {
+		cout << "Ingrese el valor de n: ";
+		cin  >> n;
+	} while (n<0); // La condicion debe contemplar los valores incorrectos
+	               // Negar La condicion correcta
+	for (i=2; i<=n; i++)
+		factorial = factorial * i; // Acumulador de producto
+	cout << "El Factorial de  " << n << " es: " << factorial;
+	getch();
+}
+
